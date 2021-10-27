007a10d7dbb7d2575287b415cdd7d48f3c4f093a
gael <gaelmbourou11@gmail.com>
2021-10-27 12:51:15 +0200

    intégration du composant réseaux sociaux

---
+++ components/block_contents/SocialMediaNav.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="mb-4 pt-3" >
+        <h5>Nos résaux sociaux</h5>
+        <b-avatar variant="primary" text="BV" size="2rem"></b-avatar>
+        <b-avatar variant="info" src="https://placekitten.com/300/300" size="2rem"></b-avatar>
+        <b-avatar variant="success" icon="people-fill" size="2rem"></b-avatar>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'SocialMediaNav',
+    }
+</script>
