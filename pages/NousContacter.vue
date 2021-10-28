a1f5f1f788d6121bc1c4798628ba329cc6bb82c2
gael <gaelmbourou11@gmail.com>
2021-10-28 18:15:37 +0200

    Ajout des métas données Titre - et description sur la page de contact

--- pages/NousContacter.vue
+++ pages/NousContacter.vue
@@ -8,6 +8,21 @@
 import TopNavBar from '../components/bar_de_navication/TopNavBar.vue'
 export default{
     name:'NousContacter',
+         head() {
+      // Configurer les méta tags pour cette page
+      return{
+        title:"Contacter une association d'aide pour autiste",
+         meta:[
+          {name:"description",content:this.myDescription}
+        ],
+
+      }
+    },
+       data(){
+      return{
+        myDescription:"Association capable mets à votre disposition des tarifs adaptés à vos besoins. ces tarifs sont fonction de votre situation et nous vous conseillons pour l'obtension des aides",
+      }
+    },
      components:{
         TopNavBar
     }
