47cdbb471d53e1468b19c0ce5c7628fdde2c5cc1
gael <gaelmbourou11@gmail.com>
2021-10-28 18:16:52 +0200

    Ajout des métas données Titre - et description sur la page de présentant les aides Financières

--- pages/AideFinanciere.vue
+++ pages/AideFinanciere.vue
@@ -8,6 +8,20 @@
 import TopNavBar from '../components/bar_de_navication/TopNavBar.vue'
 export default{
     name:'AideFinanciere',
+         head() {
+      // Configurer les méta tags pour cette page
+      return{
+        title:"Financement pour personnes avec troubles du specter autistique",
+        meta:[
+          {name:"description",content:this.myDescription}
+        ],
+      }
+    },
+       data(){
+      return{
+        myDescription:"Obtenez toutes les informations concernant les aides des proposé aux personnes avec trouble du spectre autistique ou pour personnes agées",
+      }
+    },
         components:{
         TopNavBar
     }
