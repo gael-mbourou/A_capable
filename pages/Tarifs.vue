497f31d43b2f4aab5fa740aa557673704699b387
gael <gaelmbourou11@gmail.com>
2021-10-28 18:12:43 +0200

    Ajout des métas données Titre - et description sur la page Des Tarifs

--- pages/Tarifs.vue
+++ pages/Tarifs.vue
@@ -8,6 +8,20 @@
 import TopNavBar from '../components/bar_de_navication/TopNavBar.vue'
 export default{
     name:'Tarifs',
+         head() {
+      // Configurer les méta tags pour cette page
+      return{
+        title:"Tarifs pour pour accompagnement des personnes avec trouble autistique",
+        meta:[
+          {name:"description",content:this.myDescription}
+        ],
+      }
+    },
+    data(){
+      return{
+        myDescription:"Association capable mets à votre disposition des tarifs adaptés à vos besoins. ces tarifs sont fonction de votre situation et nous vous conseillons pour l'obtension des aides",
+      }
+    },
      components:{
         TopNavBar
     }
